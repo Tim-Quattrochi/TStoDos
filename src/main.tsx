c7d8b578d1e97c7ffd39e7fdd935b6b0c8882303
Tim Quattrochi <timq82@gmail.com>
2024-03-13 20:57:20 -0400

    took out browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { TodoProvider } from "./context/TodoContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
-  </BrowserRouter>
+  <TodoProvider>
+    <App />
+  </TodoProvider>
 );
